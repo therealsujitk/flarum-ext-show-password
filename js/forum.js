82dcc600528efe18902f4769ad03f313f2bd1c4a
Sujit Kumar <60378235+therealsujitk@users.noreply.github.com>
2020-02-07 21:41:25 +0530

    Initial commit

--- js/forum.js
+++ js/forum.js
@@ -36,14 +36,4 @@ app.initializers.add('therealsujitk/flarum-ext-show-password', () => {
       10
     );
   });
-
-  extend(ChangeEmailModal.prototype, 'fields', function (fields) {
-    fields.add('therealsujitk-show-password', m('label.checkbox[style = padding-bottom: 7px]', [
-      m('input', {
-        type: 'checkbox',
-        onclick: () => {showPassword()}
-      }),
-      app.translator.trans('therealsujitk.forum.show_password_label')])
-    );
-  });
 });
